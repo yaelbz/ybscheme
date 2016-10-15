67a424d9ddd3f27dace7077e399bd07c50c214ed
yael <yael@yaels-macbook.fritz.box>
2016-10-15 17:52:08 +0200

    added lambda tests

--- test/BuiltinSyntaxTests.cpp
+++ test/BuiltinSyntaxTests.cpp
@@ -67,6 +67,7 @@ public:
 	}
 };
 
+// #### if #######################################################################################
 
 
 TEST_F(BuiltinSyntaxTests, If_WorksWith3ArgsTrue) {
@@ -136,3 +137,52 @@ TEST_F(BuiltinSyntaxTests, If_eqOp2Numbers) {
 	EXPECT_TRUE(o->u.number.isInteger);
 	EXPECT_EQ(1, (o->u.number.value.i));
 }
+
+// #### lambda #######################################################################################
+
+// > ((lambda (x) x) 10)
+// 10
+TEST_F(BuiltinSyntaxTests, lambda_returnParameter) {
+
+	writeln("((lambda (x) x) 10)");
+
+	OBJ ro = ybRead(stdin);
+	//ybPrint(ro);
+	OBJ o  = ybEval(NULL, ro);
+	//ybPrint(o);
+
+	EXPECT_EQ(T_NUMBER, TYPE(o));
+	EXPECT_TRUE(o->u.number.isInteger);
+	EXPECT_EQ(10, (o->u.number.value.i));
+}
+
+// > ((lambda (x y) (* x y)) 10 2)
+// 20
+TEST_F(BuiltinSyntaxTests, lambda_2paramsReturnMult) {
+
+	writeln("((lambda (x y) (* x y)) 10 2)");
+
+	OBJ ro = ybRead(stdin);
+	//ybPrint(ro);
+	OBJ o  = ybEval(NULL, ro);
+	//ybPrint(o);
+
+	EXPECT_EQ(T_NUMBER, TYPE(o));
+	EXPECT_TRUE(o->u.number.isInteger);
+	EXPECT_EQ(20, (o->u.number.value.i));
+}
+
+// > ((lambda (x y) (list y x)) 1 2)
+// '(2 1)
+
+
+// > ((lambda (x [y 5]) (list y x)) 1 2)
+// '(2 1)
+
+/*
+> (let ([f (lambda (x #:arg y) (list y x))])
+   (list (f 1 #:arg 2)
+         (f #:arg 2 1)))
+
+'((2 1) (2 1))
+*/
